5cb77fc579d9b7829d7edaf1a737bc89015854d2
Raffaele Prunella <Gind@MacBook-Pro-di-Raffaele.local>
2015-05-31 21:04:05 +0200

    My message

---
+++ AppTabacchi/ViewHealt.swift
@@ -0,0 +1,13 @@
+//
+//  ViewHealt.swift
+//  AppTabacchi
+//
+//  Created by Raffaele Prunella on 30/05/15.
+//  Copyright (c) 2015 Raffaele Prunella. All rights reserved.
+//
+
+import Cocoa
+
+class ViewHealt: UIViewController {
+
+}
---
+++ AppTabacchi/ViewMaps.swift
@@ -0,0 +1,9 @@
+//
+//  ViewMaps.swift
+//  AppTabacchi
+//
+//  Created by Raffaele Prunella on 30/05/15.
+//  Copyright (c) 2015 Raffaele Prunella. All rights reserved.
+//
+
+import Foundation
